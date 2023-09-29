27fd8e2884f1bd8e6023e7cbdc1456447095ba39
sunilcostacloud <sunil@costacloud.com>
2023-09-29 12:06:35 +0530

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'webpack.config.js', 'node_modules'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'webpack.config.js', 'node_modules', 'bootstrap.js'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
